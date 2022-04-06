cf986218c3e72b07066625e4c19dabe85af78a3b
Hilary112 <hilarybarry98@gmail.com>
2022-04-06 18:59:00 +0100

    assignment three

--- assignments/assignment-2/components/footer.php
+++
@@ -1,5 +0,0 @@
-<?php echo '      
-      <footer class="m-4">
-        <p class="text-center text-muted">&copy; Assignment 2</p>
-      </footer>'
-?>
--- assignments/assignment-2/components/nav-menu.php
+++
@@ -1,7 +0,0 @@
-<?php echo '
-  <ul class="nav my-nav bg-light">
-  <li class="nav-item"><a class="nav-link" href="./wireframe.php">Wireframe</a></li>
-    <li class="nav-item"><a class="nav-link" href="./document.php">Document</a></li>
-  </ul>
-';
-?>
